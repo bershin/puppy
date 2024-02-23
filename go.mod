de6d53bc2c9d47d2b514e022227b35904b952ae0
bjohn <bjohn@rocketlawyer.co.uk>
2024-02-23 13:00:03 +0000

    Initial

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bershin/puppy
 
 go 1.21.4
+
+require github.com/bershin/dog v0.0.0-20240223124539-391dc30bdc9a // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/bershin/dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof Woof!!"
+}
+
+func BigBark() string {
+	return dog.Bark(Bark())
+}
+
+func BigBarks() string {
+	return dog.Bark(Barks())
 }
